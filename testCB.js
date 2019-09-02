e75929aa667ab53dcb9e602ac56e924b1e0530ff
alinsoft <54784888+alinsoft@users.noreply.github.com>
2019-09-02 04:46:25 +0300

    Create testCB.js

---
+++ testCB.js
@@ -0,0 +1,439 @@
+
+function Batau() {
+  var copyText = document.getElementById("Batau");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("batau");
+  tooltip.innerHTML = "Copiat  "+ copyText.value;
+  }
+  
+  function Borza() {
+  var copyText = document.getElementById("Borza");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("borza");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+function Burcea() {
+  var copyText = document.getElementById("Burcea");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("burcea");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+function Cainaru() {
+  var copyText = document.getElementById("Cainaru");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("cainaru");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+function Calivascu() {
+  var copyText = document.getElementById("Calivascu");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("calivascu");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+function DumitruA() {
+  var copyText = document.getElementById("DumitruA");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("dumitrua");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function DumitruG() {
+  var copyText = document.getElementById("DumitruG");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("dumitrug");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function Glont() {
+  var copyText = document.getElementById("Glont");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("glont");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function GrosuD() {
+  var copyText = document.getElementById("GrosuD");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("grosud");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function GrosuI() {
+  var copyText = document.getElementById("GrosuI");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("grosui");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function Khalaf() {
+  var copyText = document.getElementById("Khalaf");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("khalaf");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function Marculescu() {
+  var copyText = document.getElementById("Marculescu");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("marculescu");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function Moise() {
+  var copyText = document.getElementById("Moise");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("moise");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function Nedelciu() {
+  var copyText = document.getElementById("Nedelciu");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("nedelciu");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function Olteanu() {
+  var copyText = document.getElementById("Olteanu");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("olteanu");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function Serban() {
+  var copyText = document.getElementById("Serban");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("serban");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function SimionG() {
+  var copyText = document.getElementById("SimionG");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("simiong");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function Stefan() {
+  var copyText = document.getElementById("Stefan");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("stefan");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function Tihulca() {
+  var copyText = document.getElementById("Tihulca");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("tihulca");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function VargaS() {
+  var copyText = document.getElementById("VargaS");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("vargas");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function Yudy() {
+  var copyText = document.getElementById("Yudy");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("yudy");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function Bandi() {
+  var copyText = document.getElementById("Bandi");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("bandi");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function Costache() {
+  var copyText = document.getElementById("Costache");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("costache");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function Matescu() {
+  var copyText = document.getElementById("Matescu");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("matescu");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function Nane() {
+  var copyText = document.getElementById("Nane");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("nane");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function Pavel() {
+  var copyText = document.getElementById("Pavel");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("pavel");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function Sandu() {
+  var copyText = document.getElementById("Sandu");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("sandu");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function Savu() {
+  var copyText = document.getElementById("Savu");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("savu");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function Apostu() {
+  var copyText = document.getElementById("Apostu");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("apostu");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function Dobranis() {
+  var copyText = document.getElementById("Dobranis");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("dobranis");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function Gavrilita() {
+  var copyText = document.getElementById("Gavrilita");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("gavrilita");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function Mocondreanu() {
+  var copyText = document.getElementById("Mocondreanu");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("mocondreanu");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function Neagu() {
+  var copyText = document.getElementById("Neagu");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("neagu");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function Onofrei() {
+  var copyText = document.getElementById("Onofrei");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("onofrei");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function Opincariu() {
+  var copyText = document.getElementById("Opincariu");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("opincariu");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function Popescu() {
+  var copyText = document.getElementById("Popescu");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("popescu");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function Rusu() {
+  var copyText = document.getElementById("Rusu");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("rusu");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function Salajan() {
+  var copyText = document.getElementById("Salajan");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("salajan");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function Vatavu() {
+  var copyText = document.getElementById("Vatavu");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("vatavu");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+function Voinea() {
+  var copyText = document.getElementById("Voinea");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  var tooltip = document.getElementById("voinea");
+  tooltip.innerHTML = "Copiat  "+ copyText.value ;  
+}
+
+function outFunc() {
+  var tooltip = document.getElementById("Batau");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("Borza");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("Burcea");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("Cainaru");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("Calivascu");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("DumitruA");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("DumitruG");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("Glont");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("GrosuD");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("grosui");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("khalaf");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("marculescu");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("moise");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("nedelciu");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("olteanu");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("serban");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("simiong");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("stefan");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("tihulca");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("vargas");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("yudy");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("bandi");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("costache");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("matescu");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("nane");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("pavel");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("sandu");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("savu");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("apostu");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("dobranis");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("gavrilita");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("mocondreanu");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("neagu");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("onofrei");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("opincariu");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("Popescu");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("rusu");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("salajan");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("vatavu");
+  tooltip.innerHTML = "Copiaza username-ul";
+  var tooltip = document.getElementById("voinea");
+  tooltip.innerHTML = "Copiaza username-ul";
+}
+
